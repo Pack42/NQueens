3416a7ad2de491b224e27803baf88192543abd60
Ninjoesweg <Josephpack91@gmail.com>
2024-08-08 17:47:05 -0400

    Added increase n button

--- Visualize.py
+++ Visualize.py
@@ -1,10 +1,27 @@
 from NQueens import NQueens
 import tkinter as tk
 
+# Create the main window
+root = tk.Tk()
+root.title("Visualization for nqueens puzzle")
+
+# Define initial variables
+cell_width = 5
+cell_height = 2
+n = 4
+
+
+# Function to change the grid size and redraw
+def increaseN():
+    global n
+    n += 1  # Increase grid size by 1
+    create_grid(n, getBoard())  # Recreate grid
+
 def create_grid(n, queens):
-    # Create the main window
-    root = tk.Tk()
-    root.title("Visualization for nqueens puzzle")
+    for widget in root.winfo_children():
+        widget.destroy()
+    button = tk.Button(root, text="Increase N", command=increaseN)
+    button.pack()
 
     # Create a frame to hold the grid
     frame = tk.Frame(root)
@@ -20,37 +37,25 @@ def create_grid(n, queens):
                 color = "#000000"
             else:
                 color = colors[(i + j) % 2]  # Alternate colors
-            cell = tk.Label(frame, width=5, height=2, bg=color, borderwidth=1, relief="solid")
+            cell = tk.Label(frame, width=cell_width, height=cell_height, bg=color, borderwidth=1, relief="solid")
             cell.grid(row=i, column=j, padx=1, pady=1)
 
-    # Start the Tkinter event loop
-    root.mainloop()
 
-# While loop to get user input
-while True:
-    # Get input
-    user = input("Choose size: ")
-    # Try to convert to number
-    try:
-        n = int(user)
-        # If successful but number is less than 4, print error and ask again
-        if n < 4:
-            print("Error size must be at least 4")
-        # If successful and number is greater than 4, break out of loop
-        else:
-            break
-    # If unsuccessful, print error and ask again
-    except ValueError:
-        print("Error unrecognized input")
+def getBoard():
+    # If n % 6 = 2 then call runTwo with param n
+    if (n % 6 == 2):
+        board = nq.runTwo(n)
+    # If n % 6 = 3 then call runThree with param n
+    elif (n % 6 == 3):
+        board = nq.runThree(n)
+    # Otherwise call runOther with param n
+    else:
+        board = nq.runOther(n)
+    return board
+
+# Create nqueens variable to get board
 nq = NQueens()
-# If n % 6 = 2 then call runTwo with param n
-if (n % 6 == 2):
-    board = nq.runTwo(n)
-# If n % 6 = 3 then call runThree with param n
-elif (n % 6 == 3):
-    board = nq.runThree(n)
-# Otherwise call runOther with param n
-else:
-    board = nq.runOther(n)
 # Call create grid
-create_grid(n, board)
+create_grid(n, getBoard())
+# Start the Tkinter event loop
+root.mainloop()
